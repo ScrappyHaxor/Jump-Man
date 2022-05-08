51417db818b4452496d7e8e93cc2e5d02bcf1cb2
Zeradamuz <robin@pamo.se>
2022-05-08 17:14:35 +0200

    Illusion platform

---
+++ JumpMan/Objects/IllusionPlatform.cs
@@ -0,0 +1,83 @@
+﻿using JumpMan.ECS.Components;
+using Microsoft.Xna.Framework;
+using ScrapBox.Framework.ECS;
+using ScrapBox.Framework.ECS.Components;
+using ScrapBox.Framework.Level;
+using ScrapBox.Framework.Managers;
+using ScrapBox.Framework.Math;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static ScrapBox.Framework.ECS.Collider;
+
+namespace JumpMan.Objects
+{
+    public class IllusionPlatform : Entity
+    {
+        public override string Name => "Tileable Platform";
+
+        public Transform Transform;
+        public Sprite2D Sprite;
+        public RigidBody2D Rigidbody;
+                
+
+        public IllusionPlatform(string texture, ScrapVector position, ScrapVector dimensions) : base(SceneManager.CurrentScene.Stack.Fetch(DefaultLayers.FOREGROUND))
+        {
+            Transform = new Transform
+            {
+                Position = position,
+                Dimensions = dimensions
+            };
+
+            RegisterComponent(Transform);
+
+            Rigidbody = new RigidBody2D
+            {
+                IsStatic = true,
+                
+            };
+
+            RegisterComponent(Rigidbody);
+
+
+            Sprite = new Sprite2D
+            {
+                Texture = AssetManager.FetchTexture(texture),
+                Mode = SpriteMode.TILE
+            };
+
+            RegisterComponent(Sprite);
+        }
+
+        public override void Awake()
+        {
+            base.Awake();
+        }
+
+        public override void Sleep()
+        {
+            base.Sleep();
+        }
+
+        public override void PreLayerTick(double dt)
+        {
+            base.PreLayerTick(dt);
+            
+        }
+
+        public override void PostLayerTick(double dt)
+        {
+            base.PostLayerTick(dt);
+        }
+
+        public override void PreLayerRender(Camera camera)
+        {
+            base.PreLayerRender(camera);
+        }
+
+        public override void PostLayerRender(Camera camera)
+        {
+            base.PostLayerRender(camera);
+        }
+    }
+}
