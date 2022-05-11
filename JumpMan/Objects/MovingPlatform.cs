3b3f44943829dac8dedaecb004b1740614162e1e
Zeradamuz <robin@pamo.se>
2022-05-11 15:34:04 +0200

    Create MovingPlatform.cs

---
+++ JumpMan/Objects/MovingPlatform.cs
@@ -0,0 +1,109 @@
+﻿using JumpMan.ECS.Components;
+using Microsoft.Xna.Framework;
+using ScrapBox.Framework.ECS;
+using ScrapBox.Framework.ECS.Components;
+using ScrapBox.Framework.Level;
+using ScrapBox.Framework.Managers;
+using ScrapBox.Framework.Math;
+using ScrapBox.Framework.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static ScrapBox.Framework.ECS.Collider;
+
+namespace JumpMan.Objects
+{
+    public class MovingPlatform : Entity
+    {
+        public override string Name => "Tileable Platform";
+
+        public Transform Transform;
+        public Sprite2D Sprite;
+        public RigidBody2D Rigidbody;
+        public BoxCollider2D Collider;
+        public Player player;
+        bool success;
+
+        public ScrapVector startPosition;
+        ScrapVector vector = new ScrapVector(1, 0);
+        public MovingPlatform(string texture, ScrapVector position, ScrapVector dimensions) : base(SceneManager.CurrentScene.Stack.Fetch(DefaultLayers.FOREGROUND))
+        {
+            Transform = new Transform
+            {
+                Position = position,
+                Dimensions = dimensions
+            };
+
+            startPosition = position;
+            RegisterComponent(Transform);
+
+            Rigidbody = new RigidBody2D
+            {
+                IsStatic = true
+            };
+
+            RegisterComponent(Rigidbody);
+
+            Collider = new BoxCollider2D
+            {
+                Algorithm = CollisionAlgorithm.SAT,
+                Dimensions = dimensions
+            };
+
+            RegisterComponent(Collider);
+
+            Sprite = new Sprite2D
+            {
+                Texture = AssetManager.FetchTexture(texture),
+                Mode = SpriteMode.TILE
+            };
+
+            RegisterComponent(Sprite);
+        }
+
+        public override void Awake()
+        {
+            success = Dependency<Player>(out player);
+            base.Awake();
+        }
+
+        public override void Sleep()
+        {
+            base.Sleep();
+        }
+
+        public override void PreLayerTick(double dt)
+        {
+            base.PreLayerTick(dt);
+            
+            Transform.Position += vector;
+            if (Transform.Position.X < startPosition.X - 100)
+            {
+                vector = new ScrapVector(1, 0);
+            }
+            else if(Transform.Position.X > startPosition.X + 100)
+            {
+                vector = new ScrapVector(-1, 0);
+            }
+            if (Collision.IntersectPolygons(Collider.GetVerticies(), player.Collider.GetVerticies(), out CollisionManifold manifold) && success)
+            {
+                player.Transform.Position += vector;
+            }
+        }
+
+        public override void PostLayerTick(double dt)
+        {
+            base.PostLayerTick(dt);
+        }
+
+        public override void PreLayerRender(Camera camera)
+        {
+            base.PreLayerRender(camera);
+        }
+
+        public override void PostLayerRender(Camera camera)
+        {
+            base.PostLayerRender(camera);
+        }
+    }
+}
